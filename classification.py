4a4b297d2e55015157e06a7a48bbe2ee8a32fa29
hatimchachuliya <hatim03chachuliya@gmail.com>
2018-07-31 00:43:57 +0530

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,42 +1,48 @@
 
-# coding: utf-8
-
-# In[17]:
-
-
 from sklearn import tree
-'''DATA is been stored or given here'''
-x=[[181,80,44],[177,70,43],[161,70,44],[176,80,40],[160,50,30],[165,60,34],[141,60,44],[185,80,43],[178,76,42],[171,60,44],[176,65,42],[178,87,44],[181,70,40],[154,77,43],[181,80,44],[187,88,46]]
-y=['male','female','female','male','female','male','male','female','male','female','male','female','female','male','male','female']
-
-'''defining the variable to store the decision tree model'''
-
-clf=tree.DecisionTreeClassifier()
-
-'''trains the data'''
-clf=clf.fit(x,y) 
-predicition=clf.predict([[190,70,44]])
-print(predicition)
-
-
-# In[18]:
-
-
-from sklearn.neighbors import KNeighborsClassifier
-clf=clf.fit(x,y) 
-predicition=clf.predict([[190,70,44]])
-print(predicition)
-
-
-# In[16]:
-
-
 from sklearn.svm import SVC
-clf = SVC()
-clf.fit(x, y)
-SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-    decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
-    max_iter=-1, probability=False, random_state=None, shrinking=True,
-    tol=0.001, verbose=False)
-print(clf.predict([[190,70,44]]))
+from sklearn.linear_model import Perceptron
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import numpy as np
 
+# Data and labels
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
+
+# Classifiers
+# using the default values for all the hyperparameters
+clf_tree = tree.DecisionTreeClassifier()
+clf_svm = SVC()
+clf_perceptron = Perceptron()
+clf_KNN = KNeighborsClassifier()
+
+# Training the models
+clf_tree.fit(X, Y)
+clf_svm.fit(X, Y)
+clf_perceptron.fit(X, Y)
+clf_KNN.fit(X, Y)
+
+# Testing using the same data
+pred_tree = clf_tree.predict(X)
+acc_tree = accuracy_score(Y, pred_tree) * 100
+print('Accuracy for DecisionTree: {}'.format(acc_tree))
+
+pred_svm = clf_svm.predict(X)
+acc_svm = accuracy_score(Y, pred_svm) * 100
+print('Accuracy for SVM: {}'.format(acc_svm))
+
+pred_per = clf_perceptron.predict(X)
+acc_per = accuracy_score(Y, pred_per) * 100
+print('Accuracy for perceptron: {}'.format(acc_per))
+
+pred_KNN = clf_KNN.predict(X)
+acc_KNN = accuracy_score(Y, pred_KNN) * 100
+print('Accuracy for KNN: {}'.format(acc_KNN))
+
+# The best classifier from svm, per, KNN
+index = np.argmax([acc_svm, acc_per, acc_KNN])
+classifiers = {0: 'SVM', 1: 'Perceptron', 2: 'KNN'}
+print('Best gender classifier is {}'.format(classifiers[index]))
